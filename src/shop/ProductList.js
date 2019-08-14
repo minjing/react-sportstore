168d0bcb23bf2a2cbe72e3b8244e2d4c7bd0f889
Min <min_jing@aliyun.com>
2019-08-14 17:04:54 +0800

    Add code to show products by category

--- src/shop/ProductList.js
+++ src/shop/ProductList.js
@@ -5,11 +5,11 @@ export class ProductList extends Component {
         if (this.props.products == null || this.props.products.length === 0) {
             return <h5 className="p-2">No Products</h5>
         }
-        return this.props.Products.map(p =>
+        return this.props.products.map(p =>
             <div className="card m-1 p-1 bg-light" key={p.id}>
                 <h4>
                     { p.name }
-                    <span className="badge badge-pill-primary float-right">
+                    <span className="badge badge-pill badge-primary float-right">
                         ${ p.price.toFixed(2) }
                     </span>
                 </h4>
